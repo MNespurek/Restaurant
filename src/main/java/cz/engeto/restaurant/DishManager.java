9f4aa9692f522ab45b37404c8320528e92a78b2e
michal.nespurek <michal.nespurek@email.cz>
2024-03-26 10:54:10 +0100

    Creating Class DishManager for CRUD dishes

---
+++ src/main/java/cz/engeto/restaurant/DishManager.java
@@ -0,0 +1,13 @@
+package cz.engeto.restaurant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DishManager {
+    private List<Dish> dishStack = new ArrayList<>();
+
+    public void addDish(Dish dish) {
+
+    }
+
+}
