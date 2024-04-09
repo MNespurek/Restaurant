d1c027975ed97b87f8f8bc676eb8a5474681d628
michal.nespurek <michal.nespurek@email.cz>
2024-04-09 12:32:14 +0200

    function for loading order list added

--- src/main/java/cz/engeto/restaurant/RestaurantManager.java
+++ src/main/java/cz/engeto/restaurant/RestaurantManager.java
@@ -1,8 +1,10 @@
 package cz.engeto.restaurant;
 
 import java.io.*;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class RestaurantManager {
 
@@ -34,4 +36,33 @@ public class RestaurantManager {
             throw new RestaurantException("Chyba při načítání souboru "+file+ e.getLocalizedMessage()+".");
         }
     }
+
+    public void loadOrderList(String file) throws RestaurantException, IOException {
+        orderList.clear();
+        int lineCounter = 0;
+        try(Scanner scanner = new Scanner(new BufferedReader(new FileReader(file)))) {
+            while(scanner.hasNextLine()) {
+                lineCounter++;
+                String sentence = scanner.nextLine();
+                String[] sentenceDevided = sentence.split(Settings.getDelimiter());
+                int dishId = Integer.parseInt(sentenceDevided[0]);
+                DishManager dishManager = new DishManager();
+                Boolean isDish = dishManager.isDishById(dishId);
+                if(isDish) {
+                    Dish dish = dishManager.chosenDish(dishId);
+                    Integer dishAmount = Integer.parseInt(sentenceDevided[1]);
+                    LocalTime orderTime = LocalTime.parse(sentenceDevided[2]);
+                    LocalTime fulfilmentTime = LocalTime.parse(sentenceDevided[3]);
+                    Boolean alreadyPayed = Boolean.parseBoolean(sentenceDevided[4]);
+                    Order order = new Order(dish, dishAmount, orderTime, fulfilmentTime, alreadyPayed);
+                    orderList.add(order);
+                }
+                else throw new RestaurantException("jídlo s Id" +dishId+ "nebylo nalezeno, není tudíž možné vytvořit objednávku");
+                }
+        }catch (FileNotFoundException e) {
+            throw new RestaurantException("Soubor" +file+ "se nepovedlo najít."+e.getLocalizedMessage());
+        }
+    }
 }
+
+
