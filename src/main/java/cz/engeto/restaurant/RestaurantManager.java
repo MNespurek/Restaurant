ddcec6fe3ab2a78799d25bc84f4ee6d97591576f
michal.nespurek <michal.nespurek@email.cz>
2024-04-17 17:34:24 +0200

    creating methods for management

--- src/main/java/cz/engeto/restaurant/RestaurantManager.java
+++ src/main/java/cz/engeto/restaurant/RestaurantManager.java
@@ -2,9 +2,7 @@ package cz.engeto.restaurant;
 
 import java.io.*;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class RestaurantManager {
 
@@ -19,9 +17,41 @@ public class RestaurantManager {
         tablesList.add(table);
     }
 
+    public String getOrdersBeforeFulfilmentTime() throws IOException, RestaurantException {
+        Integer notFulfilmentTimeTableOrders = 0;
+        List<Tables> sortedTableList = new ArrayList<>(tablesList);
+        for(Tables table : sortedTableList) {
+            table.loadOrderList();
+            Integer numberOfNotFulfilmentTimeOrders = table.getNotFulfilmentTimeOrders();
+            notFulfilmentTimeTableOrders =+ numberOfNotFulfilmentTimeOrders;
 
+            }
+        return "Počet nedokončených objednávek je: "+notFulfilmentTimeTableOrders+".";
+    }
 
+    public String averageGetReadyOrders() throws IOException, RestaurantException {
 
+        List<Tables> sortedTableList = new ArrayList<>(tablesList);
+
+        Long totalPreparationTime = 0L;
+        Long totalNumberOfOrders = 0L;
+
+        for(Tables table : sortedTableList) {
+            table.loadOrderList();
+            Long numberOfOrders = (long) table.getOrderList().size();
+            totalNumberOfOrders += numberOfOrders;
+            List<Long> listOfTotalPreparationTime = table.getListOfTotalPreparationTime();
+            totalPreparationTime += table.getTotalPreparationTime(listOfTotalPreparationTime);
+        }
+        return "Průměrná doba realizace objednávky je: "+totalPreparationTime/totalNumberOfOrders+ ".";
+    }
 }
+
+
+
+
+
+
+
 
 
