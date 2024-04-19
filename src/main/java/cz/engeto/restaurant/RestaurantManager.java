7199899d7070ed2bf3f9780454ab35e3cc4816d5
michal.nespurek <michal.nespurek@email.cz>
2024-04-19 15:27:41 +0200

    create a method for allOrders of the app tables

--- src/main/java/cz/engeto/restaurant/RestaurantManager.java
+++ src/main/java/cz/engeto/restaurant/RestaurantManager.java
@@ -7,51 +7,59 @@ import java.util.*;
 public class RestaurantManager {
 
 
-    private List<Tables> tablesList = new ArrayList<>();
+    private List<Table> tablesList = new ArrayList<>();
 
-    public List<Tables> getTableList() {
+    public List<Table> getTableList() {
         return tablesList;
     }
 
-    public void addTable(Tables table) {
+    public void addTable(Table table) {
         tablesList.add(table);
     }
 
     public String getOrdersBeforeFulfilmentTime() throws IOException, RestaurantException {
         Integer notFulfilmentTimeTableOrders = 0;
-        List<Tables> sortedTableList = new ArrayList<>(tablesList);
-        for(Tables table : sortedTableList) {
+        List<Table> sortedTableList = new ArrayList<>(tablesList);
+        for (Table table : sortedTableList) {
             table.loadOrderList();
             Integer numberOfNotFulfilmentTimeOrders = table.getNotFulfilmentTimeOrders();
-            notFulfilmentTimeTableOrders =+ numberOfNotFulfilmentTimeOrders;
+            notFulfilmentTimeTableOrders = +numberOfNotFulfilmentTimeOrders;
 
-            }
-        return "Počet nedokončených objednávek je: "+notFulfilmentTimeTableOrders+".";
+        }
+        return "Počet nedokončených objednávek je: " + notFulfilmentTimeTableOrders + ".";
     }
 
     public String averageGetReadyOrders() throws IOException, RestaurantException {
 
-        List<Tables> sortedTableList = new ArrayList<>(tablesList);
+        List<Table> sortedTableList = new ArrayList<>(tablesList);
 
         Long totalPreparationTime = 0L;
         Long totalNumberOfOrders = 0L;
 
-        for(Tables table : sortedTableList) {
+        for (Table table : sortedTableList) {
             table.loadOrderList();
             Long numberOfOrders = (long) table.getOrderList().size();
             totalNumberOfOrders += numberOfOrders;
             List<Long> listOfTotalPreparationTime = table.getListOfTotalPreparationTime();
             totalPreparationTime += table.getTotalPreparationTime(listOfTotalPreparationTime);
         }
-        return "Průměrná doba realizace objednávky je: "+totalPreparationTime/totalNumberOfOrders+ ".";
+        try {
+            return "Průměrná doba realizace objednávky je: " + totalPreparationTime / totalNumberOfOrders + ".";
+        } catch (ArithmeticException e) {
+            throw new RestaurantException("Došlo k chybě při dělení nulou: " + e.getLocalizedMessage());
+        }
     }
+
+    public List<Order> allOrders() throws IOException, RestaurantException {
+        List<Table> sortedTableList = new ArrayList<>(tablesList);
+        List<Order> notSortedOrderTimeList = new ArrayList<>();
+        for (Table table : sortedTableList) {
+            table.loadOrderList();
+            List<Order> orderList = table.getOrdersSortedBeginOrderedTime();
+            notSortedOrderTimeList.addAll(orderList);
+        }
+        return notSortedOrderTimeList;
+    }
+
+
 }
-
-
-
-
-
-
-
-
-
